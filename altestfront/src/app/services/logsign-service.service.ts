ef932404b4d168a4d4b46b967215b1f0c2240c80
William Daza <williamdaza04@gmail.com>
2023-06-09 15:14:41 -0500

    fix service

--- altestfront/src/app/services/logsign-service.service.ts
+++ altestfront/src/app/services/logsign-service.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LogsignServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  postSignUp(data:object){
-    const url = 'URL_DEL_ENDPOINT';
-
-  this.http.post(url, data).subscribe(
-    (response) => {
-      console.log('Solicitud POST exitosa:', response);
-    },
-    (error) => {
-      console.error('Error en la solicitud POST:', error);
-    }
-  );
+  postSignUp(data: object) {
+    const url = 'http://localhost:3000/user/create-user';
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+  
+    this.http.post(url, JSON.stringify(data), { headers }).subscribe(
+      (response) => {
+        console.log('Solicitud POST exitosa:', response);
+      },
+      (error) => {
+        console.error('Error en la solicitud POST:', error);
+      }
+    );
   }
 }
